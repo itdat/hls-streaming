0442cf275233ee8f62f20340ef3fe6da8cf4b2fa
Nguyen Tuan Dat <53074785+itdat@users.noreply.github.com>
2023-07-02 21:34:00 +0700

    init

---
+++ app.js
@@ -0,0 +1,40 @@
+var http = require("http");
+var fs = require("fs");
+
+const port = 8080;
+
+http
+  .createServer(function (request, response) {
+    console.log("request starting...");
+
+    var filePath = "." + request.url;
+    console.log(filePath);
+
+    fs.readFile(filePath, function (error, content) {
+      response.writeHead(200, { "Access-Control-Allow-Origin": "*" });
+      if (error) {
+        if (error.code == "ENOENT") {
+          fs.readFile("./404.html", function (error, content) {
+            response.end(content, "utf-8");
+          });
+        } else {
+          response.writeHead(500);
+          response.end(
+            "Sorry, check with the site admin for error: " +
+              error.code +
+              " ..\n"
+          );
+          response.end();
+        }
+      } else {
+        response.writeHead(200, {
+          "Content-Type": "application/vnd.apple.mpegurl",
+          "Access-Control-Allow-Origin": "*",
+        });
+        const stream = fs.createReadStream(filePath);
+        stream.pipe(response);
+      }
+    });
+  })
+  .listen(port);
+console.log(`Server running at http://127.0.0.1:${port}/`);
